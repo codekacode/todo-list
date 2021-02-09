b2c4b5e417d1e3682cc115ccb8b9f6cffc0be204
Codekacode <karem.quiroz.m@gmail.com>
2021-02-09 00:04:32 -0500

    Add some text of list to do with icon  to test

--- app.js
+++ app.js
@@ -13,5 +13,21 @@ function addTodo(event) {
   event.preventDefault();
   const todoDiv = document.createElement('div');
   todoDiv.classList.add('todo')
-
+  //Create li
+  const newTodo = document.createElement('li');
+  newTodo.innerText = 'hey'
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  //Check mark button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //Check trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-check"></i>';
+  trashButton.classList.add("complete-btn");
+  todoDiv.appendChild(trashButton);
+  //Append to list
+  todoList.appendChild(todoDiv);
 }
